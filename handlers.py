ee53bee4a9030749a49a95ff8eba1a4aa9b4478e
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2023-08-20 22:00:17 +0100

    added diagnostics. typing effect

--- handlers.py
+++ handlers.py
@@ -1,3 +1,6 @@
+import time
+delay = 0.1
+
 def internet_radio():
     # Code to handle Internet Radio action
     pass
@@ -13,3 +16,18 @@ def casting():
 def podcasts():
     # Code to handle Podcasts action
     pass
+
+def load_ascii_art(filename):
+    # Load the ASCII art from the file
+    with open(filename, 'r') as file:
+        return file.read()
+
+def type_out(stdscr, y, x, text, color=None, delay=0.01):
+    for char in text:
+        if color:
+            stdscr.addch(y, x, char, color)
+        else:
+            stdscr.addch(y, x, char)
+        x += 1  # move the x-coordinate for the next character
+        stdscr.refresh()  # update the screen
+        time.sleep(delay)  # introduce a delay
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import curses
 import json
-from handlers import internet_radio, spotify, casting, podcasts
+from handlers import internet_radio, spotify, casting, podcasts, load_ascii_art, type_out
 
 
 # Load the JSON menu
@@ -11,6 +11,9 @@ with open('menu.json', 'r') as file:
 # Define some padding
 pad_top = 5
 pad_left = 10
+menu_start_position = 5
+
+
 
 
 # The dispatch table mapping action names to functions
@@ -22,43 +25,62 @@ action_dispatch = {
     "exit_program": exit
 }
 
-ascii_art = """
-    _   _           _       
-   | | | |         | |      
-   | |_| | __ _ ___| |_ ___ 
-   |  _  |/ _` / __| __/ _ \\
-   | | | | (_| \__ \ ||  __/
-   \_| |_/\__,_|___/\__\___|
-"""
+header_art = load_ascii_art("header.txt")
+
 
 def main(stdscr):
+    #menu drawn bool so only runs once
+    menu_drawn = False
+    diag_drawn = False
     curses.start_color()
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.curs_set(0)
-
     current_row = 0
 
     while True:
         stdscr.clear()
         stdscr.bkgd(curses.color_pair(1))
 
-        # Display ASCII art
-        for line_idx, line in enumerate(ascii_art.split('\n')):
-            stdscr.addstr(line_idx, pad_left, line, curses.color_pair(1))
+        # Display the ASCII art at the very top
+        stdscr.addstr(0, 0, header_art)
         
-        menu_start_position = len(ascii_art.split('\n'))
+        
 
         # Display menu items below the ASCII art
-        for idx, item in enumerate(main_menu_items):
-            if idx == current_row:
-                stdscr.addstr(pad_top + menu_start_position + idx, pad_left, item["name"], curses.A_REVERSE | curses.color_pair(1))
-            else:
-                stdscr.addstr(pad_top + menu_start_position + idx, pad_left, item["name"], curses.color_pair(1))
+        if not menu_drawn:
+            for idx, item in enumerate(main_menu_items):
+                if idx == current_row:
+                    type_out(stdscr, pad_top + menu_start_position + idx, pad_left, item["name"], curses.A_REVERSE | curses.color_pair(1))
+                else:
+                    type_out(stdscr, pad_top + menu_start_position + idx, pad_left, item["name"], curses.color_pair(1))
+            menu_drawn = True
+        else:
+            for idx, item in enumerate(main_menu_items):
+                if idx == current_row:
+                    stdscr.addstr(pad_top + menu_start_position + idx, pad_left, item["name"], curses.A_REVERSE | curses.color_pair(1))
+                else:
+                    stdscr.addstr(pad_top + menu_start_position + idx, pad_left, item["name"], curses.color_pair(1))
+
+        # Display fake diagnostic text in the second column
+        if not diag_drawn:
+            diagnostic_start_x = pad_left + 20  # Adjust the starting x-coordinate as needed
+            type_out(stdscr, pad_top + menu_start_position, diagnostic_start_x, "| System Info", 1)
+            type_out(stdscr, pad_top + menu_start_position + 1, diagnostic_start_x, "| CPU: 23%", 1)
+            type_out(stdscr, pad_top + menu_start_position + 2, diagnostic_start_x, "| RAM: 1.2 GB", 1)
+            type_out(stdscr, pad_top + menu_start_position + 3, diagnostic_start_x, "| NET: UP", 1)
+            diag_drawn = True
+        else:
+            diagnostic_start_x = pad_left + 20  # Adjust the starting x-coordinate as needed
+            stdscr.addstr(pad_top + menu_start_position, diagnostic_start_x, "System Info", curses.color_pair(1))
+            stdscr.addstr(pad_top + menu_start_position + 1, diagnostic_start_x, "CPU: 23%", curses.color_pair(1))
+            stdscr.addstr(pad_top + menu_start_position + 2, diagnostic_start_x, "RAM: 1.2 GB", curses.color_pair(1))
+            stdscr.addstr(pad_top + menu_start_position + 3, diagnostic_start_x, "NET: UP", curses.color_pair(1))
+            
+
+# Wait for user input
+
 
         key = stdscr.getch()
-        
-
-
         if key == curses.KEY_UP and current_row > 0:
             current_row -= 1
         elif key == curses.KEY_DOWN and current_row < len(main_menu_items) - 1:
